5a495e2c8e5a3324feb1c7d0cf2125e09890c219
Ilfat Sayakhov <sayahov1998@gmail.com>
2020-12-10 20:16:31 +0300

    Initial commit

---
+++ src/main/java/com/kpfu/consumer/course_notifications/CourseNotificationsApplication.java
@@ -0,0 +1,14 @@
+package com.kpfu.consumer.course_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CourseNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/kpfu/consumer/course_notifications/components/ConsumersComponent.java
@@ -0,0 +1,73 @@
+package com.kpfu.consumer.course_notifications.components;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ConsumersComponent {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private List<KafkaMessageListenerContainer<Integer, String>> containers = new ArrayList<>();
+
+    @PostConstruct
+    public void init() throws InterruptedException {
+        log.info("Start init containers");
+        containers.add(startContainer("topic_1", "topic_2"));
+        containers.add(startContainer("topic_2", "topic_3"));
+    }
+
+    private KafkaMessageListenerContainer<Integer, String> startContainer(String... topics) {
+        int num = containers.size();
+        ContainerProperties containerProps = new ContainerProperties(topics);
+        containerProps.setMessageListener((MessageListener<Integer, String>) message -> {
+            log.info("received from container" + num + " : "  + message);
+        });
+        KafkaMessageListenerContainer<Integer, String> container = createContainer(containerProps);
+        container.setBeanName("testAuto");
+        container.start();
+        return container;
+    }
+
+    private KafkaMessageListenerContainer<Integer, String> createContainer(
+            ContainerProperties containerProps) {
+        Map<String, Object> props = consumerProps();
+        DefaultKafkaConsumerFactory<Integer, String> cf =
+                new DefaultKafkaConsumerFactory<>(props);
+        KafkaMessageListenerContainer<Integer, String> container =
+                new KafkaMessageListenerContainer<>(cf, containerProps);
+        return container;
+    }
+
+    private Map<String, Object> consumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "main_group");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return props;
+    }
+}
